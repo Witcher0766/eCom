3e4945d49267827607c7f35933aefd01431bcbda
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2023-08-05 20:04:23 +0530

    changes

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 const app = express();
 app.use(cors())
 app.use(express.json())
-const path = require("path")
+// const path = require("path")
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static("build"))
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"))
-  })
-}
+// if(process.env.NODE_ENV === "production") {
+//   app.use(express.static("build"))
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "build", "index.html"))
+//   })
+// }
 
 
 app.get("/", (req, res) => {
